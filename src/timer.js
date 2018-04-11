2c9edb3ed195aeefb37b189ead549ae92595959b
Held Grijo <shadyueh@gmail.com>
2018-04-11 02:36:13 -0400

    Chronometer adjustments and starting inline edit

--- src/timer.js
+++ src/timer.js
@@ -1,20 +1,77 @@
 window.onload = function () {
 
     var fragmentTime;
+    var timeIntervalID;
+    var txtMinutes;
+    var txtSeconds;
+    var btnPlay = '#timer-play';
+    var btnStop = "#timer-stop";
+    var sound = new Howl({
+        src: ['PowerStar.mp3']
+    });
 
-    jQuery('.timeout_message_show').hide();
 
-    var txtMinutes = jQuery('span.minute').text();
-    var txSeconds = jQuery('span.second').text();
-
-
-
+    function getDisplay() {
+        txtMinutes = document.querySelector('span.minute').textContent;
+        txtSeconds = document.querySelector('span.second').textContent;
+    }
 
 
-    document.querySelector("#play").onclick =
+    function setDisplay(txtMin, txtSec) {
+        document.querySelector('span.minute').textContent = txtMin;
+        document.querySelector('span.second').textContent = txtSec;
+    }
+
+
+    function playAlarm() {
+        sound.loop(true);
+        sound.play();
+    }
+
+
+    function stopAlarm() {
+        sound.stop();
+    }
+
+
+    function startTimer(duration, displayMinute, displaySecond) {
+
+        var timer = duration;
+
+        timeIntervalID = setInterval(function () {
+
+            if (--timer < 0) {
+                timer = 0;
+                clearInterval(timeIntervalID);
+                playAlarm();
+                document.querySelector(btnPlay).disabled = false;
+                alert(jQuery('.timeout_message_show').text());
+            }
+
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            displayMinute.textContent = minutes;
+            displaySecond.textContent = seconds;
+
+        }, 1000);
+
+    }
+
+    //button play listener
+    document.querySelector(btnPlay).onclick =
         function () {
+            //disable button
+            this.disabled = true;
+
+            getDisplay();
+
+            //parse time text
             var minutes = parseInt(txtMinutes);
-            var seconds = parseInt(txSeconds);
+            var seconds = parseInt(txtSeconds);
+
+            //values validation
             if (isNaN(minutes)) {
                 minutes = 00;
             }
@@ -30,47 +87,19 @@ window.onload = function () {
             if (seconds == 60) {
                 seconds = 59;
             }
-            setDisplay(txtMinutes,txSeconds);
+
             fragmentTime = (60 * minutes) + (seconds);
             displayMinute = document.querySelector('span.minute');
             displaySecond = document.querySelector('span.second');
+
             startTimer(fragmentTime, displayMinute, displaySecond);
-        }
+        };
 
-};
-
-function getDisplay(txtMin,txSec) {
-    jQuery('span.minute').text(txtMin);
-    jQuery('span.second').text(txSec);
-}
-
-
-function setDisplay(txtMin,txSec) {
-    jQuery('span.minute').text(txtMin);
-    jQuery('span.second').text(txSec);
-}
-
-
-function startTimer(duration, displayMinute, displaySecond) {
-
-    var timer = duration, displayMinute, displaySecond;
-
-    var timeIntervalID = setInterval(function () {
-
-        if (--timer < 0) {
-            timer = 0;
+    document.querySelector(btnStop).onclick =
+        function () {
+            stopAlarm();
             clearInterval(timeIntervalID);
-            this.disabled = 'false';
-            alert(jQuery('.timeout_message_show').text());
-        }
-
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        displayMinute.textContent = minutes;
-        displaySecond.textContent = seconds;
-
-    }, 1000);
-
-}
+            setDisplay(txtMinutes, txtSeconds);
+            document.querySelector(btnPlay).disabled = false;
+        };
+};
