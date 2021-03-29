9d2f3532b7b150b199c9d307b684f62cb0681cb5
Held Grijo <held.grijo@gmail.com>
2021-03-29 18:13:31 -0400

    Updating to use Next.js

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Timer from "../components/Timer";
+
+function IndexPage() {
+  return (
+    <div className='timer-container' id='#run-the-timer'>
+      <h1>6º Agile Meetup Manaus</h1>
+
+      <Timer />
+    </div>
+  );
+}
+
+export default IndexPage;
