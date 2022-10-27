80a22f8400f022494ecfe7684cbf6140c013ea8e
Held Grijo <held.grijo@manaus.am.gov.br>
2022-10-27 16:37:27 -0400

    chore: update project for security improvement

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-// require('dotenv').config()
-
-module.exports = {
-  // env: {
-  //   GA_TRACKING_ID: process.env.GA_TRACKING_ID,
-  // },
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    
-    return config
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
+
+module.exports = nextConfig
